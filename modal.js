aa3370e5be648bf1bb1e456a684e542d134f19b7
yaredtt <yaredtekle22@gmail.com>
2023-01-12 16:46:16 +0300

    add event listener for list and contact section

--- modal.js
+++ modal.js
@@ -1,6 +1,6 @@
 let listSection = document.querySelector(".list-section")
 let addSection = document.querySelector(".add-section")
-let contactSection = document.querySelector(".contact-section")
+let contactSection = document.querySelector(".contact")
 let listButton = document.querySelector(".list-button")
 let addButton = document.querySelector(".add-button")
 let contactButton = document.querySelector(".contact-button")
@@ -9,5 +9,13 @@ function showList(){
     console.log('none')
 };
 listButton.addEventListener("click", (e) => {
-    showList();
+    listSection.style.display = "none";
+
 });
+
+contactSection.addEventListener('click', (e) => {
+    listSection.style.display = 'none';
+    addSection.style.display = 'none';
+    contactSection.style.display = 'flex'
+});
+
