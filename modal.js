53625104380d9a2ce5c7ee4deda566c85dfed789
cindykandie <cindyjk6@gmail.com>
2023-01-12 17:39:22 +0300

    added event listenter to list, add, contact links

--- modal.js
+++ modal.js
@@ -1,21 +1,39 @@
 let listSection = document.querySelector(".list-section")
 let addSection = document.querySelector(".add-section")
-let contactSection = document.querySelector(".contact")
+let contactSection = document.querySelector(".contact-section")
 let listButton = document.querySelector(".list-button")
 let addButton = document.querySelector(".add-button")
 let contactButton = document.querySelector(".contact-button")
+
 function showList(){
-    listSection.style.display = "none"
-    console.log('none')
+    addSection.classList.add("modal-disappear")
+    contactSection.classList.add("modal-disappear")
+    listSection.classList.add("modal-appear")
+    console.log('list')
+};
+function showAdd(){
+    addSection.classList.add("modal-appear")
+    contactSection.classList.add("modal-disappear")
+    listSection.classList.add("modal-disappear")
+    console.log('add')
+};
+function showContact(){
+    addSection.classList.add("modal-disappear")
+    contactSection.classList.add("modal-appear")
+    listSection.classList.add("modal-disappear")
+    console.log('contact')
 };
 listButton.addEventListener("click", (e) => {
-    listSection.style.display = "none";
+    showList()
 
 });
+addButton.addEventListener("click", (e) => {
+    showAdd()
 
-contactSection.addEventListener('click', (e) => {
-    listSection.style.display = 'none';
-    addSection.style.display = 'none';
-    contactSection.style.display = 'flex'
 });
+contactButton.addEventListener("click", (e) => {
+    showContact()
+});
+
+
 
